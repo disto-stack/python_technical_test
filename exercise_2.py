23d8ec84fb18b1eb4ea8e8011e1380cbf90badd8
disto-stack <juan.ocampo.2501@gmail.com>
2022-06-21 15:42:21 -0500

    exercise 2 completed

---
+++ exercise_2.py
@@ -0,0 +1,17 @@
+# Este ejemplo toma una lista de numero e imprime el menor entre todos ellos
+numeros = input("Ingrese la lista de números separada por comas: ")
+
+# Convierte la cadena numeros en una lista, y se inicializa una variable sumatoria en cero
+lista_numeros = numeros.split(',')
+sumatoria = 0
+
+# Se recorre la lista de numeros
+for numero in lista_numeros:
+  numero = int(numero) # Se hace el parseo para evitar errores
+  sumatoria += numero * numero # Se suman los cuadrados de cada numero
+
+# Se imprime la sumatoria
+print(sumatoria)
+  
+
+
