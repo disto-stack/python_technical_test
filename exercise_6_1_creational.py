d81d16e85ddb6d2387066d2eb5f9fa75edebf476
disto-stack <juan.ocampo.2501@gmail.com>
2022-06-22 23:05:21 -0500

    exercise 6.1 completed

---
+++ exercise_6_1_creational.py
@@ -0,0 +1,36 @@
+# En este ejemplo voy a explicar e implementar un patron de diseño, el elegido es Singleton
+
+'''
+  Es un patron que grantiza la creacion de una sola instancia de una clase a lo largo del ciclo 
+  de vida de una aplicacion. Mas en https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide
+'''
+
+# Creamos la clase, en este caso la clase verificará si hay una instancia de la misma, en caso de haberla
+# se retornara la instancia ya creada.
+class ClaseSingleton():
+  '''
+    El metodo __new__ es un metodo estático perteneciente a la clase de un objeto, 
+    __new__ se ejecuta antes de __init__, siempre lo hace, lo que significa que si no lo declaramos, 
+    este se ejecuta de manera explícita, el primer argumento (cls) se refiere a la clase del objeto
+  '''
+  def __new__(cls):
+    if not hasattr(cls, 'instance'): # Verificamos si existe una instancia de la clase
+      cls.instance = super().__new__(cls) # En caso de no haber una instancia, se crea una nueva
+
+    return cls.instance # Se retorna la instancia, ya sea una nueva o una definida anteriormente (Depende del condicional anterior)
+
+
+# Para demostrar que son las mismas instancias, crearemos dos objetos
+singleton = ClaseSingleton()
+nuevo_singleton = ClaseSingleton()
+
+
+# Se verifica las instancias, si esta correcta la implementacion del patron, debe dar la salida:
+# 'Las dos clases tienen la misma instancia'
+if (singleton is nuevo_singleton):
+  print('Las dos clases tienen la misma instancia')
+else:
+  print('Las dos clases no tienen la misma instancia')
+  
+
+
